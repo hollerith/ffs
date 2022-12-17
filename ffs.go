5aec78eefbf1474ad771d1d7907e0b560177f463
dev <nmous@oznz.me>
2022-12-17 18:07:38 +0000

    various bugs, root parms, stats

--- ffs.go
+++ ffs.go
@@ -7,26 +7,48 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"strconv"
 
 	"github.com/spf13/pflag"
 )
 
+func replaceNonPrintable(s string) string {
+	b := []byte(s)
+	for i, c := range b {
+		if !strconv.IsPrint(rune(c)) {
+			b[i] = '.'
+		}
+	}
+	return string(b)
+}
+
 func main() {
 	var filePatternRegex *regexp.Regexp
-	var contentsPatternRegex *regexp.Regexp
+	var stringPatternRegex *regexp.Regexp
+    var filePattern string
+    var stringPattern string
+    var hexPattern string
 	var verbose bool
 	var binary bool
-	var root string // variable to hold the value of the root parameter
+	var root string
+    var fileCount int
+    var matchCount int
 
 	pflag.StringP("file", "f", "", "regex pattern to match file names")
-	pflag.StringP("match", "m", "", "regex pattern to match file contents")
+	pflag.StringVarP(&stringPattern,"string", "s", "", "regex pattern to match file string")
+	pflag.StringVarP(&hexPattern,"hex", "h", "", "regex pattern to match hex-encoded lines")
+	pflag.StringVarP(&root, "root", "r", ".", "root directory to start the search from")
 	pflag.BoolP("verbose", "v", false, "enable verbose mode")
 	pflag.BoolP("binary", "b", false, "include binary files in search")
-	pflag.StringVarP(&root, "root", "r", ".", "root directory to start the search from") // add the root parameter
-	pflag.Parse()
+    pflag.Parse()
+	
+    rootArgs := pflag.Args()
+    if len(rootArgs) > 0 {
+        root = rootArgs[0]
+    } else {
+        root = "."
+    }
 
-	filePattern := pflag.Lookup("file").Value.String()
-	contentsPattern := pflag.Lookup("match").Value.String()
 	verbose = pflag.Lookup("verbose").Value.String() == "true"
 	binary = pflag.Lookup("binary").Value.String() == "true"
 
@@ -38,20 +60,32 @@ func main() {
 			os.Exit(1)
 		}
 	}
-	if contentsPattern == "" {
-		fmt.Println("Error: contents pattern regex is required")
+	if stringPattern == "" && hexPattern == "" {
+		fmt.Println("Error: string or hex pattern regex is required")
 		os.Exit(1)
 	}
+
 	var err error
-	contentsPatternRegex, err = regexp.Compile(contentsPattern)
+	stringPatternRegex, err = regexp.Compile(stringPattern)
 	if err != nil {
-		fmt.Println("Error compiling contents pattern regex:", err)
+		fmt.Println("Error compiling string pattern regex:", err)
 		os.Exit(1)
+	}
+	var hexPatternRegex *regexp.Regexp
+	if hexPattern != "" {
+		var err error
+		hexPatternRegex, err = regexp.Compile(hexPattern)
+		if err != nil {
+			fmt.Println("Error compiling hex pattern regex:", err)
+			os.Exit(1)
+		}
 	}
 
 	err = filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			fmt.Printf("Error processing file %s: %v\n", path, err)
+            if verbose {
+                fmt.Printf("Error processing file %s: %v\n", path, err)
+            }    
 			return nil
 		}
 
@@ -65,18 +99,36 @@ func main() {
 
 		if verbose {
 			fmt.Println("Matching file:", path)
+			fileCount++
 		}
 
 		file, err := os.Open(path)
 		if err != nil {
-			fmt.Printf("Error opening file %s: %v\n", path, err)
+            if verbose {
+                fmt.Printf("Error opening file %s: %v\n", path, err)
+            }            
 			return nil
 		}
 		defer file.Close()
 
 		// Check if file is binary and skip if not set to include binary files
 		if !binary {
-			head := make([]byte, 512) // read the first 512 bytes of the file
+			// Get file size
+			fileInfo, err := file.Stat()
+			if err != nil {
+                if verbose {
+                    fmt.Printf("Error getting file info for %s: %v\n", path, err)
+                }                
+				return nil
+			}
+
+			// Determine number of bytes to read
+			numBytes := fileInfo.Size()
+			if numBytes > 512 {
+				numBytes = 512
+			}
+
+			head := make([]byte, numBytes) // read the head
 			_, err = file.Read(head)
 			if err != nil {
 				fmt.Printf("Error reading file %s: %v\n", path, err)
@@ -84,11 +136,16 @@ func main() {
 			}
 			// check if there are any nulbytes in the head of the file
 			if bytes.Contains(head, []byte{0}) {
+                if verbose {
+                    fmt.Printf("Skipping binary file %s\n", path)
+                }        
 				return nil
 			}
 			_, err = file.Seek(0, 0) // reset file pointer to the beginning of the file
 			if err != nil {
-				fmt.Printf("Error resetting file pointer for %s: %v\n", path, err)
+                if verbose {
+                    fmt.Printf("Error resetting file pointer for %s: %v\n", path, err)
+                }        
 				return nil
 			}
 		}
@@ -98,20 +155,57 @@ func main() {
 		lineNumber := 1
 		for scanner.Scan() {
 			line := scanner.Text()
-			if contentsPatternRegex.MatchString(line) {
-				fmt.Printf("%s:%d:%s\n", path, lineNumber, line)
+			var match bool
+			if hexPatternRegex != nil {
+				// Convert line to hex string and perform match on hex string
+				hex := ""
+				for _, b := range line {
+					hex += " " + strconv.FormatInt(int64(b), 16)
+				}
+				match = hexPatternRegex.MatchString(hex)
+			} else {
+				match = stringPatternRegex.MatchString(line)
+			}
+			if match {
+				matchCount++
+				if verbose {
+					fmt.Printf("%s:%d:%s\n", path, lineNumber, replaceNonPrintable(line))
+				} else {
+					fmt.Printf("%s\n", path)
+				}           
 			}
 			lineNumber++
 		}
 		if err := scanner.Err(); err != nil {
-			fmt.Printf("Error scanning file %s: %v\n", path, err)
+            if verbose {
+                fmt.Printf("Error scanning file %s: %v\n", path, err)
+            }            
 			return nil
 		}
 
 		return nil
 	})
-
 	if err != nil {
-		fmt.Printf("Error walking directories: %v\n", err)
+        if verbose {
+            fmt.Printf("Error walking directories: %v\n", err)
+        }        
 	}
+
+    if verbose {
+        fmt.Println("Options chosen:")
+        if filePattern != "" {
+            fmt.Println("- file pattern:", filePattern)
+        }
+        if stringPattern != "" {
+            fmt.Println("- string pattern:", stringPattern)
+        }
+        if hexPattern != "" {
+            fmt.Println("- hex pattern:", hexPattern)
+        }
+        fmt.Println("- root directory:", root)
+        fmt.Println("- binary files:", binary)
+        fmt.Println("Number of matches:")
+        fmt.Println("- files:", fileCount)
+        fmt.Println("- matches:", matchCount)
+    }	
 }
