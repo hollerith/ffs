f70478e1e18818900723fe94926d01d8572b9f00
hollerith <hollerith+github@gmail.com>
2023-06-16 11:51:26 +0100

    pretty colors and code reorg

--- ffs.go
+++ ffs.go
@@ -182,7 +182,7 @@ func main() {
 				if match {
 					matchCount++
 					if verbose {
-						fmt.Printf("%s:%d:%s\n", path, lineNumber, replaceNonPrintable(line))
+						fmt.Printf("\x1b[38;5;221m%s\x1b[0m:\x1b[38;5;39m%d\x1b[0m:\x1b[38;5;8m%s\x1b[0m\n", path, lineNumber, replaceNonPrintable(line))
 					}
 				}
 				lineNumber++
@@ -197,71 +197,12 @@ func main() {
 
 		// Print results
 		if (matchCount > lastCount) || (stringPatternRegex == nil && hexPatternRegex == nil && metaPatternRegex == nil) {
-
-			// Print directory
-			if fileCount == 0 || directory != lastDir {
-				lastDir = directory
-				// Check if the directory is a symlink
-				dirInfo, err := os.Lstat(lastDir)
-				if err == nil && dirInfo.Mode()&os.ModeSymlink == os.ModeSymlink {
-					// directory is a symlink, print final path in light yellow with arrow pointing to actual path in regular green
-					finalPath, err := filepath.EvalSymlinks(lastDir)
-					if err != nil {
-						fmt.Printf("\n\033[38;5;221m%s\033[0m (could not resolve symlink):\n", lastDir)
-					} else {
-						fmt.Printf("\n\033[38;5;221m%s\033[0m --> \033[32m%s\033[0m:\n", lastDir, finalPath)
-					}
-				} else {
-					fmt.Printf("\n\033[32m%s\033[0m:\n", lastDir)
-				}
-			}
-
-			// Print the current file details
-			sizeStr := fmt.Sprintf("%*d", sizeWidth, metaData.Size)
-			modeStr := formatColumn(metaData.Mode, modeWidth)
-			if metaData.Suid {
-				modeStr = fmt.Sprintf("\x1b[31m%s\x1b[0m", modeStr)
-			}
-			ownerStr := formatColumn(metaData.Owner, ownerWidth)
-			groupStr := formatColumn(metaData.Group, groupWidth)
-			timeStr := formatColumn(metaData.ModTime, timeWidth)
-			mimeTypeStr := formatColumn(metaData.MimeType, mimeTypeWidth)
-			fileStr := filename
-			if metaData.Link != "" {
-				// file is a link, color it light yellow
-				fileStr = fmt.Sprintf("\x1b[38;5;221m%s\x1b[0m --> %s", filename, metaData.Link)
-			} else {
-				if fi.Mode().Perm()&0111 != 0 {
-					if fi.Mode().Perm()&0007 != 0 {
-						// file is world executable, color it dark red
-						fileStr = fmt.Sprintf("\x1b[38;5;124m%s\x1b[0m", filename)
-					} else if fi.Mode().Perm()&0070 != 0 {
-						// file is group executable, color it light red
-						fileStr = fmt.Sprintf("\x1b[38;5;211m%s\x1b[0m", filename)
-					} else {
-						// file is owner executable, color it light pink
-						fileStr = fmt.Sprintf("\x1b[38;5;219m%s\x1b[0m", filename)
-					}
-				}
-				// Exclude symlinks from byteCount
-				byteCount += metaData.Size
-			}
-
-			var errorStr string
-			if (errors) {
-				errorStr = fmt.Sprintf("\033[90m - %s\033[0m", metaData.Error)
-			}
-
-			if verbose {
-				fmt.Printf("%s %s %s %s %s %s %s %s\n", modeStr, ownerStr, groupStr, sizeStr, timeStr, mimeTypeStr, fileStr, errorStr)
-			} else {
-				fmt.Printf("%s\n", fileStr)
-			}
-			fileCount++
+			lastDir, fileCount, matchCount, byteCount = printResults(fileCount, lastDir, directory, filename, metaData, fi, byteCount, matchCount, verbose, errors)
 		}
 
 		return nil
 	})
+
 	if err != nil {
 		if errors {
 			fmt.Printf("Error walking directories: %v\n", err)
@@ -279,6 +220,73 @@ func main() {
 	}
 }
 
+func printResults(fileCount int, lastDir string, directory string, filename string, metaData Metadata, fi os.FileInfo, byteCount int64, matchCount int, verbose bool, errors bool) (string, int, int, int64) {
+	// Print directory
+	if fileCount == 0 || lastDir != directory {
+		lastDir = directory
+		// Check if the directory is a symlink
+		dirInfo, err := os.Lstat(lastDir)
+		if err == nil && dirInfo.Mode()&os.ModeSymlink == os.ModeSymlink {
+			// directory is a symlink, print final path in light yellow with arrow pointing to actual path in regular green
+			finalPath, err := filepath.EvalSymlinks(lastDir)
+			if err != nil {
+				fmt.Printf("\n\033[38;5;221m%s\033[0m (could not resolve symlink):\n", lastDir)
+			} else {
+				fmt.Printf("\n\033[38;5;221m%s\033[0m --> \033[32m%s\033[0m:\n", lastDir, finalPath)
+			}
+		} else {
+			fmt.Printf("\n\033[32m%s\033[0m:\n", lastDir)
+		}
+	}
+
+	// Print the current file details
+	sizeStr := fmt.Sprintf("%*d", sizeWidth, metaData.Size)
+	modeStr := formatColumn(metaData.Mode, modeWidth)
+	if metaData.Suid {
+		modeStr = fmt.Sprintf("\x1b[31m%s\x1b[0m", modeStr)
+	}
+	ownerStr := formatColumn(metaData.Owner, ownerWidth)
+	groupStr := formatColumn(metaData.Group, groupWidth)
+	timeStr := formatColumn(metaData.ModTime, timeWidth)
+	mimeTypeStr := formatColumn(metaData.MimeType, mimeTypeWidth)
+	fileStr := filename
+	if metaData.Link != "" {
+		// file is a link, color it light yellow
+		fileStr = fmt.Sprintf("\x1b[38;5;221m%s\x1b[0m --> %s", filename, metaData.Link)
+	} else {
+		if fi.Mode().Perm()&0111 != 0 {
+			if fi.Mode().Perm()&0007 != 0 {
+				// file is world executable, color it dark red
+				fileStr = fmt.Sprintf("\x1b[38;5;124m%s\x1b[0m", filename)
+			} else if fi.Mode().Perm()&0070 != 0 {
+				// file is group executable, color it light red
+				fileStr = fmt.Sprintf("\x1b[38;5;211m%s\x1b[0m", filename)
+			} else {
+				// file is owner executable, color it light pink
+				fileStr = fmt.Sprintf("\x1b[38;5;219m%s\x1b[0m", filename)
+			}
+		} else {
+			fileStr = fmt.Sprintf("\x1b[38;5;117m%s\x1b[0m", filename)
+		}
+		// Exclude symlinks from byteCount
+		byteCount += metaData.Size
+	}
+
+	var errorStr string
+	if errors {
+		errorStr = fmt.Sprintf("\033[90m - %s\033[0m", metaData.Error)
+	}
+
+	if verbose {
+		fmt.Printf("%s %s %s %s %s %s %s %s\n", modeStr, ownerStr, groupStr, sizeStr, timeStr, mimeTypeStr, fileStr, errorStr)
+	} else {
+		fmt.Printf("%s\n", fileStr)
+	}
+
+	fileCount++
+
+	return lastDir, fileCount, matchCount, byteCount
+}
 func replaceNonPrintable(s string) string {
 	b := []byte(s)
 	for i, c := range b {
@@ -296,87 +304,6 @@ func formatColumn(s string, width int) string {
 	} else {
 		return s[:width-3] + "..."
 	}
-}
-
-// extract file details
-func extractFileData(file *os.File) (Metadata, bool, error) {
-	var metadata Metadata
-	isBinary := false
-
-	// Get file size, mode, owner, and group
-	fileInfo, err := file.Stat()
-	if err == nil {
-		metadata.Size = fileInfo.Size()
-		metadata.Mode = fileInfo.Mode().String()
-		metadata.Suid = (fileInfo.Mode()&os.ModeSetuid) != 0 && (fileInfo.Mode()&os.ModePerm) >= 04000
-
-		// Get owner and group ids
-		uid := fileInfo.Sys().(*syscall.Stat_t).Uid
-		gid := fileInfo.Sys().(*syscall.Stat_t).Gid
-
-		// Get owner and group names
-		u, err := user.LookupId(fmt.Sprintf("%d", uid))
-		if err == nil {
-			metadata.Owner = fmt.Sprintf("%d - %s", uid, u.Username)
-		} else {
-			metadata.Owner = fmt.Sprintf("%d", uid)
-		}
-
-		g, err := user.LookupGroupId(fmt.Sprintf("%d", gid))
-		if err == nil {
-			metadata.Group = fmt.Sprintf("%d - %s", gid, g.Name)
-		} else {
-			metadata.Group = fmt.Sprintf("%d", gid)
-		}
-
-		// Get file mod time
-		modTime := fileInfo.ModTime().Format("2006-01-02 15:04:05")
-		metadata.ModTime = modTime
-	}
-
-	// Determine number of bytes to read
-	numBytes := fileInfo.Size()
-	if numBytes > 512 {
-		numBytes = 512
-	}
-
-	// Extract MIME type
-	buf := make([]byte, numBytes)
-	_, err = file.Read(buf)
-	if err != nil {
-		return metadata, isBinary, err
-	}
-	metadata.MimeType = http.DetectContentType(buf)
-
-	// Check if MIME type belongs to a group of known binary file types
-	if !strings.HasPrefix(metadata.MimeType, "text/") {
-		isBinary = true
-	}
-
-	// If the file is not an image type return without exifdata
-	if !strings.HasPrefix(metadata.MimeType, "image/") {
-		return metadata, isBinary, nil
-	}
-
-	// Extract EXIF metadata
-	file.Seek(0, 0) // reset file pointer to the beginning of the file
-
-	exifData, err := exif.Decode(file)
-	if err != nil {
-		if err == io.EOF {
-			return metadata, isBinary, fmt.Errorf("EOF reached while reading file")
-		}
-		return metadata, isBinary, err
-	}
-
-	// Convert EXIF metadata to JSON string
-	jsonByte, err := exifData.MarshalJSON()
-	if err != nil {
-		return metadata, isBinary, err
-	}
-	metadata.ExifData = string(jsonByte)
-
-	return metadata, isBinary, nil
 }
 
 func parseFlags() (bool, bool, bool, bool, bool, string, int, *regexp.Regexp, *regexp.Regexp, *regexp.Regexp, *regexp.Regexp, bool, ignore.IgnoreParser) {
@@ -544,4 +471,85 @@ func walk(filename string, linkDirname string, followLinks bool, visited map[str
 func Walk(path string, followLinks bool, walkFn filepath.WalkFunc) error {
     visited := make(map[string]bool) // create visited map
     return walk(path, path, followLinks, visited, walkFn)
+}
+
+// extract file details
+func extractFileData(file *os.File) (Metadata, bool, error) {
+	var metadata Metadata
+	isBinary := false
+
+	// Get file size, mode, owner, and group
+	fileInfo, err := file.Stat()
+	if err == nil {
+		metadata.Size = fileInfo.Size()
+		metadata.Mode = fileInfo.Mode().String()
+		metadata.Suid = (fileInfo.Mode()&os.ModeSetuid) != 0 && (fileInfo.Mode()&os.ModePerm) >= 04000
+
+		// Get owner and group ids
+		uid := fileInfo.Sys().(*syscall.Stat_t).Uid
+		gid := fileInfo.Sys().(*syscall.Stat_t).Gid
+
+		// Get owner and group names
+		u, err := user.LookupId(fmt.Sprintf("%d", uid))
+		if err == nil {
+			metadata.Owner = fmt.Sprintf("%d - %s", uid, u.Username)
+		} else {
+			metadata.Owner = fmt.Sprintf("%d", uid)
+		}
+
+		g, err := user.LookupGroupId(fmt.Sprintf("%d", gid))
+		if err == nil {
+			metadata.Group = fmt.Sprintf("%d - %s", gid, g.Name)
+		} else {
+			metadata.Group = fmt.Sprintf("%d", gid)
+		}
+
+		// Get file mod time
+		modTime := fileInfo.ModTime().Format("2006-01-02 15:04:05")
+		metadata.ModTime = modTime
+	}
+
+	// Determine number of bytes to read
+	numBytes := fileInfo.Size()
+	if numBytes > 512 {
+		numBytes = 512
+	}
+
+	// Extract MIME type
+	buf := make([]byte, numBytes)
+	_, err = file.Read(buf)
+	if err != nil {
+		return metadata, isBinary, err
+	}
+	metadata.MimeType = http.DetectContentType(buf)
+
+	// Check if MIME type belongs to a group of known binary file types
+	if !strings.HasPrefix(metadata.MimeType, "text/") {
+		isBinary = true
+	}
+
+	// If the file is not an image type return without exifdata
+	if !strings.HasPrefix(metadata.MimeType, "image/") {
+		return metadata, isBinary, nil
+	}
+
+	// Extract EXIF metadata
+	file.Seek(0, 0) // reset file pointer to the beginning of the file
+
+	exifData, err := exif.Decode(file)
+	if err != nil {
+		if err == io.EOF {
+			return metadata, isBinary, fmt.Errorf("EOF reached while reading file")
+		}
+		return metadata, isBinary, err
+	}
+
+	// Convert EXIF metadata to JSON string
+	jsonByte, err := exifData.MarshalJSON()
+	if err != nil {
+		return metadata, isBinary, err
+	}
+	metadata.ExifData = string(jsonByte)
+
+	return metadata, isBinary, nil
 }
